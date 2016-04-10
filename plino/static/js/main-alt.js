dc07cb54d1203c9e67cef6927a5ac0b53d4c2c42
Tasdik Rahman <tasdik95@gmail.com>
2016-04-10 12:04:03 +0530

    BetterUI: Another UI created using materialize
    So this UI is a better version of the materialize-UI branch
    Has a lot of good features as like being mobile first and stuff.
    
    TODO
    ====
    
    The demo area text is still small. Have to fix it in the next commit

---
+++ plino/static/js/main-alt.js
@@ -0,0 +1,81 @@
+
+
+
+// Page Loader
+$(window).load(function() {
+  "use strict";
+  $('#loader').fadeOut();
+});
+
+jQuery(function($) {
+
+    //Initiat WOW JS
+    new WOW().init();
+
+    // one page navigation 
+    $('.main-navigation').onePageNav({
+            currentClass: 'active'
+    });
+
+    // Countdown
+    $('#counter').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+jQuery(function($) {
+
+    var bodyEl = document.body,
+        content = document.querySelector( '.contents' ),
+        openbtn = document.getElementById( 'open-button' ),
+        closebtn = document.getElementById( 'close-button' ),
+        isOpen = false;
+
+    function init() {
+        initEvents();
+    }
+
+    function initEvents() {
+        openbtn.addEventListener( 'click', toggleMenu );
+        if( closebtn ) {
+            closebtn.addEventListener( 'click', toggleMenu );
+        }
+    }
+
+    function toggleMenu() {
+        if( isOpen ) {
+            classie.remove( bodyEl, 'show-menu' );
+        }
+        else {
+            classie.add( bodyEl, 'show-menu' );
+        }
+        isOpen = !isOpen;
+    }
+
+    init();
+
+    });
+
+});
---
+++ plino/static/js/main.js
@@ -0,0 +1,81 @@
+
+jQuery(function($) {
+
+	//Initiat WOW JS
+	new WOW().init();
+
+    // one page navigation 
+    $('.main-navigation').onePageNav({
+            currentClass: 'active'
+    });
+
+    // Countdown
+	$('#counter').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.timer').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	});
+
+
+/**
+ * main.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( '.contents' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
+
+});
